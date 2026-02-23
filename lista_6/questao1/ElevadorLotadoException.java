b0d3de51b0068ab1c3cd527d5dd90effe17d10b8
joseclaudioals <128101526+joseclaudioals@users.noreply.github.com>
2026-02-23 20:37:24 -0300

    Questao 1 - Nao finalizada | Classe elevador com erros | Classe teste nao concluida

---
+++ lista_6/questao1/ElevadorLotadoException.java
@@ -0,0 +1,7 @@
+package lista_6.questao1;
+
+public class ElevadorLotadoException extends Exception {
+    public ElevadorLotadoException(String message){
+        super(message);
+    }
+}
---
+++ lista_6/questao1/Teste.java
@@ -0,0 +1,7 @@
+package lista_6.questao1;
+
+public class Teste {
+    public static void main(String[]args){
+
+    }
+}
